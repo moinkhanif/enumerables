386d97a4308e62bd72ccc52d3389254ea539e00c
Moin Khan <kmoin786@gmail.com>
2020-02-24 23:14:50 +0530

    add my_select method to enumerable module

--- enumerable.rb
+++ enumerable.rb
@@ -1,7 +1,8 @@
+# rubocop:disable Style/CaseEquality, Style/For, Lint/RedundantCopDisableDirective, Lint/MissingCopEnableDirective
 module Enumerable
   def my_each
     if block_given?
-      each do |i|
+      for i in self do
         yield i
       end
     else
@@ -12,7 +13,7 @@ module Enumerable
   def my_each_with_index
     if block_given?
       j = 0
-      each do |i|
+      for i in self do
         yield i, j
         j += 1
       end
@@ -21,21 +22,40 @@ module Enumerable
     end
   end
 
-  
+  def my_select
+    if block_given?
+      new_arr = []
+      for i in self do
+        new_arr.push(i) if yield i
+      end
+      new_arr
+    else
+      puts 'Please enter the block'
+    end
+  end
 end
+
+# my_array = [1, 2, 3, 4, 5, 6, 7, 8]
 
 # The following are tests for available methods:
 # Simply uncomment the required code to test
 
 # my_each
 #   Our method:
-# [1, 3, 5].my_each { |abc| p abc }
+# my_array.my_each { |abc| p abc }
 
 # Each Method
-#  [1, 2, 3, 4, 5, 6, 7, 8].each { |abc| p abc }
+#  my_array.each { |abc| p abc }
 
 #  my_each_with_index
 #   Our Method:
-# [1, 3, 5].my_each_with_index { |abc, i| p "#{i} : #{abc}" }
+# my_array.my_each_with_index { |abc, i| p "#{i} : #{abc}" }
 # Each_with_index method:
-# [1, 3, 5].each_with_index { |abc, i| p "#{i} : #{abc}" }
+# my_array.each_with_index { |abc, i| p "#{i} : #{abc}" }
+
+# my_select
+#   Our method:
+# my_array.my_select(&:even?)
+
+# select Method
+# my_array.select(&:even?)
