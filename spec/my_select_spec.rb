2db672bfb047580ed99544256a3f7f0b2071ccf1
Moin <kmoin786@gmail.com>
2020-04-03 23:20:03 +0530

    proofread and correct 'it' statement in my_select testfile

--- spec/my_select_spec.rb
+++ spec/my_select_spec.rb
@@ -4,17 +4,17 @@ require_relative '../enumerable.rb'
 RSpec.describe Enumerable do
   arr = [1, 5, 4, 2, 7, 8, 9]
   describe '#my_select' do
-    it 'retrns the array if cases are true' do
+    it 'returns the array if cases are true' do
       my_proc = proc { |num| num < 5 }
       expect(arr.my_select(&my_proc)).to eql([1, 4, 2])
     end
 
-    it 'retrns empty array if cases are true' do
+    it 'returns empty array if cases are false' do
       my_proc = proc { |num| num > 10 }
       expect(arr.my_select(&my_proc)).to eql([])
     end
 
-    it 'retrns enumerator when block not given' do
+    it 'returns enumerator when block not given' do
       expect(arr.my_select).to be_a(Enumerator)
     end
   end
