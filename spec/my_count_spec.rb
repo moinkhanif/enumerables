fa0b344441edcca87ad7c80dd136943b5daf8cac
Moin <kmoin786@gmail.com>
2020-04-07 01:17:37 +0530

    add arguement testcase to my_count rspec

--- spec/my_count_spec.rb
+++ spec/my_count_spec.rb
@@ -13,4 +13,8 @@ describe '#my_count' do
   it 'returns the number of words which length is equal to 3' do
     expect(%w[do ten win great].my_count { |word| word.length == 3 }).to eql(2)
   end
+
+  it 'returns the number of same elements in an array' do
+    expect([1, 2, 3, 4].my_count(2)).to eql(1)
+  end
 end
