089ebb2094cca6162cc0794d90210696d3aec76b
Moin <kmoin786@gmail.com>
2020-04-07 01:21:53 +0530

    named tests appropriately for more clarity in my_select rspec

--- spec/my_select_spec.rb
+++ spec/my_select_spec.rb
@@ -4,12 +4,12 @@ require_relative '../enumerable.rb'
 RSpec.describe Enumerable do
   arr = [1, 5, 4, 2, 7, 8, 9]
   describe '#my_select' do
-    it 'returns the array if cases are true' do
+    it 'returns the array if some cases are true' do
       my_proc = proc { |num| num < 5 }
       expect(arr.my_select(&my_proc)).to eql([1, 4, 2])
     end
 
-    it 'returns empty array if cases are false' do
+    it 'returns empty array if all cases are false' do
       my_proc = proc { |num| num > 10 }
       expect(arr.my_select(&my_proc)).to eql([])
     end
