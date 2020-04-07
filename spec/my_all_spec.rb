796e824c1c42502ad322c06bb05871cb9d04bac1
Moin <kmoin786@gmail.com>
2020-04-07 19:58:48 +0530

    replace pairs with true/false to avoid confusion

--- spec/my_all_spec.rb
+++ spec/my_all_spec.rb
@@ -2,12 +2,12 @@ require 'rspec'
 require_relative '../enumerable.rb'
 
 describe '#my_all?' do
-  it 'returns true if all elements in the array are pairs' do
+  it 'returns true if all elements in the array are true' do
     pairs = [2, 4, 6, 8, 10]
     expect(pairs.my_all?(&:even?)).to eql(true)
   end
 
-  it "returns false is all elements in the array aren't pairs" do
+  it "returns false is all elements in the array aren't true" do
     odds = [1, 3, 5, 7]
     expect(odds.my_all?(&:even?)).not_to eql(true)
   end
